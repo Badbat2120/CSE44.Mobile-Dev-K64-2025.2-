3590462f61c5269c5eff7773fc67df2ba481132a
Nguyen-Duc-Chien <2251061727@e.tlu.edu.vn>
2025-04-08 17:50:14 +0700

    Dong bo form dang ky

--- app/src/main/java/com/example/litera/views/activities/RegisterActivity.java
+++ app/src/main/java/com/example/litera/views/activities/RegisterActivity.java
@@ -2,13 +2,13 @@ package com.example.litera.views.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
 import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,11 +20,12 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
     private EditText inputEmail, inputPassword;
     private FirebaseAuth auth;
     private ProgressBar progressBar;
-    private Button btnRegister;
-    private TextView btnLogin;
+    private Button btnSignUp;
+    private TextView tvLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,17 +37,10 @@ public class RegisterActivity extends AppCompatActivity {
         inputEmail = findViewById(R.id.etEmail);
         inputPassword = findViewById(R.id.etPassword);
         progressBar = findViewById(R.id.progressBar);
-        btnRegister = findViewById(R.id.btn_register);
-        btnLogin = findViewById(R.id.tvLogin);
+        btnSignUp = findViewById(R.id.btn_register);
+        tvLogin = findViewById(R.id.tvLogin);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-            }
-        });
-
-        btnRegister.setOnClickListener(new View.OnClickListener() {
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = inputEmail.getText().toString().trim();
@@ -73,19 +67,31 @@ public class RegisterActivity extends AppCompatActivity {
                         .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                Toast.makeText(RegisterActivity.this, "User registered successfully!" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
                                 progressBar.setVisibility(View.GONE);
-
                                 if (!task.isSuccessful()) {
                                     Toast.makeText(RegisterActivity.this, "Authentication failed." + task.getException(),
                                             Toast.LENGTH_SHORT).show();
                                 } else {
-                                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                                    startActivity(new Intent(RegisterActivity.this, MainActivity.class));
                                     finish();
                                 }
                             }
                         });
             }
         });
+
+        // Làm cho chỉ chữ "Login" bấm được
+        SpannableString ss = new SpannableString("Already have an account? Login");
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        };
+
+        ss.setSpan(clickableSpan, 25, 30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE); // chỉ chữ "Login"
+        tvLogin.setText(ss);
+        tvLogin.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
