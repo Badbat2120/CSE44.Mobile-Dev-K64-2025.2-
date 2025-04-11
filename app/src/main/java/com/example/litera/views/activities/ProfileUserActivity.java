6b3b95b39e474905777bb52de3332e3625878785
Nguyen-Duc-Chien <2251061727@e.tlu.edu.vn>
2025-04-11 12:44:53 +0700

    Change password dep qua, mk phai sua moi duoc

--- app/src/main/java/com/example/litera/views/activities/ProfileUserActivity.java
+++ app/src/main/java/com/example/litera/views/activities/ProfileUserActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,13 +17,34 @@ public class ProfileUserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_user);
 
         ImageButton btnBack = findViewById(R.id.btnBack);
+        Button btnFavourites = findViewById(R.id.btnFavourites);
+        Button btnChangePass = findViewById(R.id.btnChangePass);
+
+        // Navigate back to MainActivity
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Navigate back to MainActivity
                 Intent intent = new Intent(ProfileUserActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        // Navigate to FavBookActivity
+        btnFavourites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileUserActivity.this, FavBookActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Navigate to ChangePassActivity
+        btnChangePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileUserActivity.this, ChangePassActivity.class);
+                startActivity(intent);
             }
         });
     }
